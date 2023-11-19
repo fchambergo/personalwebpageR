8d4ebf0432fe75e4187b4d40d58e8de1f25e4352
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-11-19 10:09:19 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import './App.css'
+import Navbar from './components/navbar/Nav';
+import Contact from './components/pages/contact/ContactPage';
+import Home from './components/pages/home/HomePage';
+
+
+function App() {
+
+  let Components
+  
+  switch (window.location.pathname) {
+    
+    case "/contact":
+      Components = Contact
+      break
+    default:
+      Components = Home
+      break
+    
+  }
+
+  return (
+  <>
+  <Navbar />
+  <Components />
+  
+
+
+  <Container>
+  <div class="footer">
+        <h4 class="foot-text">Made by Woody Umstead: connect via linkedin or github</h4>
+        
+        <a class='fa' href='https://www.linkedin.com/in/woody-umstead-5849a21a4/'>
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a class='fa' href='https://github.com/woodumstead'>
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
+  </Container>
+      
+  </>
+  )
+}
+
+export default App;
---
+++ src/components/navbar/Nav.js
@@ -0,0 +1,14 @@
+
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <a href="/" className="page-title">Woody Umstead</a>
+            <ul>
+                <li>
+                    <a id="contact" href="./contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
