2fc236eb76cdfa6bf6ef5f2b35b70be846f6c95f
Frank Chambergo <franky4908@gmail.com>
2023-11-22 09:04:17 -0500

    revised

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
-import { Container } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './App.css'
-import ContactLink from './components/navbar/OnClick';
 import Navbar from './components/navbar/Nav';
 import Contact from './components/pages/contact/ContactPage';
 import Home from './components/pages/home/HomePage';
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Footer from './components/footer/Footer';
+
 
 function App() {
 
-  let Components
-  
-  switch (window.location.pathname) {
-    
-    case ContactLink :
-      Components = Contact
-      break
-    default:
-      Components = Home
-      break
-    
-  }
-
   return (
-  <>
-  <Navbar />
-  <Components />
-  
-
-
-  <Container>
-  <div class="footer">
-        <h4 class="foot-text">Made by Woody Umstead: connect via linkedin or github</h4>
-        
-        <a class='fa' href='https://www.linkedin.com/in/woody-umstead-5849a21a4/'>
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a class='fa' href='https://github.com/woodumstead'>
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-      </div>
-  </Container>
-      
-  </>
-  )
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,14 +1,13 @@
-import ContactLink from "./OnClick"
+import { Outlet, NavLink } from 'react-router-dom';
 
 export default function Navbar() {
     return (
+      <>
         <nav className="nav">
-            <a href="/" className="page-title">Woody Umstead</a>
-            <ul>
-                <li>
-                    <a id="contact" onClick={ContactLink} href="./contact">Contact</a>
-                </li>
-            </ul>
+          <NavLink className="page-title" to="/">Woody Umstead</NavLink>
+          <NavLink id="contact" to="/contact">Contact</NavLink>
         </nav>
-    )
+        <Outlet />
+      </>
+    );
 }
